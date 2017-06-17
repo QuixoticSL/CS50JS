483de35c47c4ea71753e5ab3026b88166ae6601f
Zane <zanesuhi@gmail.com>
2017-06-17 11:08:40 +0200

    Updated.

--- Book 4.js
+++ Book 4.js
@@ -118,3 +118,6 @@ function foo() {
 
 var a = 1;
 foo();		// result: `undefined`, side effect: changed `a`
+
+//My take: use semicolons wherever you know they are "required,"
+// and limit your assumptions about ASI to a minimum.
