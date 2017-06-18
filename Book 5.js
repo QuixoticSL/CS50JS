b21bc44c815551bc4566657fa0f5ea5b76481174
Zane <zanesuhi@gmail.com>
2017-06-18 11:15:48 +0200

    Updated.

---
+++ Book 5.js
@@ -0,0 +1,29 @@
+// ajax(..) is some arbitrary Ajax function given by a library
+var data = ajax( "http://some.url.1" );
+
+console.log( data );
+// Oops! `data` generally won't have the Ajax results
+
+
+//Example of a simple callback function
+// ajax(..) is some arbitrary Ajax function given by a library
+ajax( "http://some.url.1", function myCallbackFunction(data){
+
+	console.log( data ); // Yay, I gots me some `data`!
+
+} );
+
+
+//Example of running code NOW and running code LATER
+function now() {
+	return 21;
+}
+
+function later() {
+	answer = answer * 2;
+	console.log( "Meaning of life:", answer );
+}
+
+var answer = now();
+
+setTimeout( later, 1000 ); // Meaning of life: 42
