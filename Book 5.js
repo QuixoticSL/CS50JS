2a373da5bd3a1b31768cb131cdbb471778b9bd6a
Zane <zanesuhi@gmail.com>
2017-06-20 18:26:41 +0200

    Updated.

--- Book 5.js
+++ Book 5.js
@@ -67,3 +67,21 @@ function later() {
 	answer = answer * 2;
 	console.log( "Meaning of life:", answer );
 }
+
+//an Ajax response handler that needs to run through a long list of results to transform the values
+var res = [];
+
+// `response(..)` receives array of results from the Ajax call
+function response(data) {
+	// add onto existing `res` array
+	res = res.concat(
+		// make a new transformed array with all `data` values doubled
+		data.map( function(val){
+			return val * 2;
+		} )
+	);
+}
+
+// ajax(..) is some arbitrary Ajax function given by a library
+ajax( "http://some.url.1", response );
+ajax( "http://some.url.2", response );
