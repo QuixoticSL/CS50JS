4975d8cdf6f10b27cc2d36ca7f09937bb38eb91b
Zane <zanesuhi@gmail.com>
2017-06-20 16:44:57 +0200

    Updated.

--- Book 5.js
+++ Book 5.js
@@ -39,3 +39,26 @@ console.log( a ); // ??
 
 // even later
 a.index++;
+
+
+//Example of event loop
+// `eventLoop` is an array that acts as a queue (first-in, first-out)
+var eventLoop = [ ];
+var event;
+
+// keep going "forever"
+while (true) {
+	// perform a "tick"
+	if (eventLoop.length > 0) {
+		// get the next event in the queue
+		event = eventLoop.shift();
+
+		// now, execute the next event
+		try {
+			event();
+		}
+		catch (err) {
+			reportError(err);
+		}
+	}
+}
