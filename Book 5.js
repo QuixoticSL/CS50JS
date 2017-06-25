9f311a7b0c42b4b85b0e0d85f0c2731cb575696e
Zane <zanesuhi@gmail.com>
2017-06-25 08:33:29 +0200

    Updated.

--- Book 5.js
+++ Book 5.js
@@ -108,3 +108,22 @@ on (foo "completion") {
 on (foo "error") {
 	// oops, something went wrong in `foo(..)`
 }
+
+function foo(x) {
+	// start doing something that could take a while
+
+	// make a `listener` event notification
+	// capability to return
+
+	return listener;
+}
+
+var evt = foo( 42 );
+
+evt.on( "completion", function(){
+	// now we can do the next step!
+} );
+
+evt.on( "failure", function(err){
+	// oops, something went wrong in `foo(..)`
+} );
