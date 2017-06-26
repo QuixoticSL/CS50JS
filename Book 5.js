5cf5c9886e8904baacb201057b82b31b6c35fd16
Zane <zanesuhi@gmail.com>
2017-06-26 10:34:43 +0200

    Updated.

--- Book 5.js
+++ Book 5.js
@@ -164,3 +164,19 @@ var p1 = new Promise(function(resolve, reject) {
 });
 
 var p2 = Promise.resolve(42);
+
+
+//Example of CHAIN FLOW with PROMISES
+var p = Promise.resolve(21);
+
+var p2 = p.then(function(v) {
+    console.log(v); // 21
+
+    // fulfill `p2` with value `42`
+    return v * 2;
+});
+
+// chain off `p2`
+p2.then(function(v) {
+    console.log(v); // 42
+});
