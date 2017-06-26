008ac7b819e43c67e42a25eab61e472300524eab
Zane <zanesuhi@gmail.com>
2017-06-26 10:29:04 +0200

    Updated.

--- Book 5.js
+++ Book 5.js
@@ -1,41 +1,41 @@
 // ajax(..) is some arbitrary Ajax function given by a library
-var data = ajax( "http://some.url.1" );
+var data = ajax("http://some.url.1");
 
-console.log( data );
+console.log(data);
 // Oops! `data` generally won't have the Ajax results
 
 
 //Example of a simple callback function
 // ajax(..) is some arbitrary Ajax function given by a library
-ajax( "http://some.url.1", function myCallbackFunction(data){
+ajax("http://some.url.1", function myCallbackFunction(data) {
 
-	console.log( data ); // Yay, I gots me some `data`!
+    console.log(data); // Yay, I gots me some `data`!
 
-} );
+});
 
 
 //Example of running code NOW and running code LATER
 function now() {
-	return 21;
+    return 21;
 }
 
 function later() {
-	answer = answer * 2;
-	console.log( "Meaning of life:", answer );
+    answer = answer * 2;
+    console.log("Meaning of life:", answer);
 }
 
 var answer = now();
 
-setTimeout( later, 1000 ); // Meaning of life: 42
+setTimeout(later, 1000); // Meaning of life: 42
 
 
 //Example of CONSOLE running in the background
 var a = {
-	index: 1
+    index: 1
 };
 
 // later
-console.log( a ); // ??
+console.log(a); // ??
 
 // even later
 a.index++;
@@ -43,29 +43,28 @@ a.index++;
 
 //Example of event loop
 // `eventLoop` is an array that acts as a queue (first-in, first-out)
-var eventLoop = [ ];
+var eventLoop = [];
 var event;
 
 // keep going "forever"
 while (true) {
-	// perform a "tick"
-	if (eventLoop.length > 0) {
-		// get the next event in the queue
-		event = eventLoop.shift();
+    // perform a "tick"
+    if (eventLoop.length > 0) {
+        // get the next event in the queue
+        event = eventLoop.shift();
 
-		// now, execute the next event
-		try {
-			event();
-		}
-		catch (err) {
-			reportError(err);
-		}
-	}
+        // now, execute the next event
+        try {
+            event();
+        } catch (err) {
+            reportError(err);
+        }
+    }
 }
 
 function later() {
-	answer = answer * 2;
-	console.log( "Meaning of life:", answer );
+    answer = answer * 2;
+    console.log("Meaning of life:", answer);
 }
 
 //an Ajax response handler that needs to run through a long list of results to transform the values
@@ -73,18 +72,18 @@ var res = [];
 
 // `response(..)` receives array of results from the Ajax call
 function response(data) {
-	// add onto existing `res` array
-	res = res.concat(
-		// make a new transformed array with all `data` values doubled
-		data.map( function(val){
-			return val * 2;
-		} )
-	);
+    // add onto existing `res` array
+    res = res.concat(
+        // make a new transformed array with all `data` values doubled
+        data.map(function(val) {
+            return val * 2;
+        })
+    );
 }
 
 // ajax(..) is some arbitrary Ajax function given by a library
-ajax( "http://some.url.1", response );
-ajax( "http://some.url.2", response );
+ajax("http://some.url.1", response);
+ajax("http://some.url.2", response);
 
 // swap `x` and `y` (via temp variable `z`)
 z = x;
@@ -93,68 +92,75 @@ y = z;
 
 var x, y = 2;
 
-console.log( x + y ); // NaN  <-- because `x` isn't set yet
+console.log(x + y); // NaN  <-- because `x` isn't set yet
 
 foo(x) {
-	// start doing something that could take a while
+    // start doing something that could take a while
 }
 
-foo( 42 )
+foo(42)
 
-on (foo "completion") {
-	// now we can do the next step!
+on(foo "completion") {
+    // now we can do the next step!
 }
 
-on (foo "error") {
-	// oops, something went wrong in `foo(..)`
+on(foo "error") {
+    // oops, something went wrong in `foo(..)`
 }
 
 function foo(x) {
-	// start doing something that could take a while
+    // start doing something that could take a while
 
-	// make a `listener` event notification
-	// capability to return
+    // make a `listener` event notification
+    // capability to return
 
-	return listener;
+    return listener;
 }
 
-var evt = foo( 42 );
+var evt = foo(42);
 
-evt.on( "completion", function(){
-	// now we can do the next step!
-} );
+evt.on("completion", function() {
+    // now we can do the next step!
+});
 
-evt.on( "failure", function(err){
-	// oops, something went wrong in `foo(..)`
-} );
+evt.on("failure", function(err) {
+    // oops, something went wrong in `foo(..)`
+});
 
 //DUCK TYPING
 if (
-	p !== null &&
-	(
-		typeof p === "object" ||
-		typeof p === "function"
-	) &&
-	typeof p.then === "function"
+    p !== null &&
+    (
+        typeof p === "object" ||
+        typeof p === "function"
+    ) &&
+    typeof p.then === "function"
 ) {
-	// assume it's a thenable!
-}
-else {
-	// not a thenable
+    // assume it's a thenable!
+} else {
+    // not a thenable
 }
 
 //A promise getting rejected.
-var p = new Promise( function(resolve,reject){
-	foo.bar();	// `foo` is not defined, so error!
-	resolve( 42 );	// never gets here :(
-} );
+var p = new Promise(function(resolve, reject) {
+    foo.bar(); // `foo` is not defined, so error!
+    resolve(42); // never gets here :(
+});
 
 p.then(
-	function fulfilled(){
-		// never gets here :(
-	},
-	function rejected(err){
-		// `err` will be a `TypeError` exception object
-		// from the `foo.bar()` line.
-	}
+    function fulfilled() {
+        // never gets here :(
+    },
+    function rejected(err) {
+        // `err` will be a `TypeError` exception object
+        // from the `foo.bar()` line.
+    }
 );
+
+
+//Example of Promis.resolve
+var p1 = new Promise(function(resolve, reject) {
+    resolve(42);
+});
+
+var p2 = Promise.resolve(42);
