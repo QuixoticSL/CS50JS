3d943c80ec0b0043b1a0c091255c7a0b8136d1a9
Zane <zanesuhi@gmail.com>
2017-07-01 08:46:35 +0200

    Updated.

--- Book 5.js
+++ Book 5.js
@@ -324,3 +324,32 @@ function run(gen) {
             })(next);
         });
 }
+
+// note: normal function, not generator
+function bar(url1, url2) {
+    return Promise.all([
+        request(url1),
+        request(url2)
+    ]);
+}
+
+function* foo() {
+    // hide the Promise-based concurrency details
+    // inside `bar(..)`
+    var results = yield bar(
+        "http://some.url.1",
+        "http://some.url.2"
+    );
+
+    var r1 = results[0];
+    var r2 = results[1];
+
+    var r3 = yield request(
+        "http://some.url.3/?v=" + r1 + "," + r2
+    );
+
+    console.log(r3);
+}
+
+// use previously defined `run(..)` utility
+run(foo);
