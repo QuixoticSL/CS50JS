6a05fd99e355caa26024b84b7f959f1965cafeee
Zane <zanesuhi@gmail.com>
2017-07-12 08:00:00 +0200

    Updated.

---
+++ Book 6.js
@@ -0,0 +1,16 @@
+// A new utility for checking strict equality of two value
+// but without the nuanced exceptions that === has for NaN and -0 values.
+if (!Object.is) {
+    Object.is = function(v1, v2) {
+        // test for `-0`
+        if (v1 === 0 && v2 === 0) {
+            return 1 / v1 === 1 / v2;
+        }
+        // test for `NaN`
+        if (v1 !== v1) {
+            return v2 !== v2;
+        }
+        // everything else
+        return v1 === v2;
+    };
+}
