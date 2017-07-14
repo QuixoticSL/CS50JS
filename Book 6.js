18ee93b830a957d451aaa7efceecfe3f342e6bcd
Zane <zanesuhi@gmail.com>
2017-07-14 16:56:38 +0200

    Updated.

--- Book 6.js
+++ Book 6.js
@@ -32,3 +32,16 @@ var a = [2, 3, 4];
 var b = [1, ...a, 5];
 
 console.log(b);
+
+
+function foo(x, y) {
+    x = x || 11;
+    y = y || 31;
+
+    console.log(x + y);
+}
+
+foo(); // 42
+foo(5, 6); // 11
+foo(5); // 36
+foo(null, 6); // 17
