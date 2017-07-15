99ce1828aa3828d9c5cad1d15ce6d185be1d3e34
Zane <zanesuhi@gmail.com>
2017-07-15 09:00:42 +0200

    Updated.

--- Book 6.js
+++ Book 6.js
@@ -46,3 +46,20 @@ foo(); // 42
 foo(5, 6); // 11
 foo(5); // 36
 foo(null, 6); // 17
+
+
+//Assignmet of default values to missing arguments
+function foo(x = 11, y = 31) {
+    console.log(x + y);
+}
+
+foo(); // 42
+foo(5, 6); // 11
+foo(0, 42); // 42
+
+foo(5); // 36
+foo(5, undefined); // 36 <-- `undefined` is missing
+foo(5, null); // 5  <-- null coerces to `0`
+
+foo(undefined, 6); // 17 <-- `undefined` is missing
+foo(null, 6); // 6  <-- null coerces to `0`
