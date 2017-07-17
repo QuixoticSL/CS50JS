78d3fb14ca3268ac56b4e9109eb594102e19fc0c
Zane <zanesuhi@gmail.com>
2017-07-17 06:21:20 +0200

    Updated.

--- Book 6.js
+++ Book 6.js
@@ -113,3 +113,19 @@ var greeting = `Hello ${name}!`;
 
 console.log(greeting); // "Hello Kyle!"
 console.log(typeof greeting); // "string"
+
+
+//Interpolated expressions
+function upper(s) {
+    return s.toUpperCase();
+}
+
+var who = "reader";
+
+var text =
+    `A very ${upper( "warm" )} welcome
+to all of you ${upper( `${who}s` )}!`;
+
+console.log( text );
+// A very WARM welcome
+// to all of you READERS!
