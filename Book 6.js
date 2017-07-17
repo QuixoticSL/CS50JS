d1d365d3f0ea3b25ab761f68251467b6c1fc9e4f
Zane <zanesuhi@gmail.com>
2017-07-17 16:01:58 +0200

    Updated.

--- Book 6.js
+++ Book 6.js
@@ -141,3 +141,13 @@ showraw`Hello\nWorld`;
 // [ "Hello
 // World" ]
 // [ "Hello\nWorld" ]
+
+console.log( `Hello\nWorld` );
+// Hello
+// World
+
+console.log( String.raw`Hello\nWorld` );
+// Hello\nWorld
+
+String.raw`Hello\nWorld`.length;
+// 12
