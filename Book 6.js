a75c0155e13e635c749e4181d69fa811f6f7da6b
Zane <zanesuhi@gmail.com>
2017-07-19 18:49:20 +0200

    Updated.

--- Book 6.js
+++ Book 6.js
@@ -177,3 +177,15 @@ var f3 = (x,y) => {
 /* Iterator [required]
     next() {method}: retrieves next IteratorResult
 */
+
+
+//Sample of a iterator function
+var arr = [1,2,3];
+
+var it = arr[Symbol.iterator]();
+
+it.next();		// { value: 1, done: false }
+it.next();		// { value: 2, done: false }
+it.next();		// { value: 3, done: false }
+
+it.next();		// { value: undefined, done: true }
